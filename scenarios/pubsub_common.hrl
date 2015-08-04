8baf501549a52c6741a0095ecbafe186ad01da62
astachurski <astachurski@gmail.com>
2015-08-04 14:32:02 +0200

    extracted common pubsub settings

---
+++ scenarios/pubsub_common.hrl
@@ -0,0 +1,3 @@
+-define(HOST, <<"xmpp.esl.com">>). %% The virtual host served by the server
+-define(SERVER_IPS, {<<"10.100.0.79">>, <<"10.100.0.80">>}). %% Tuple of servers, for example {<<"10.100.0.21">>, <<"10.100.0.22">>}
+
